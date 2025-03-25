18cb11dbadf6eceddaac4bb9152b916b9255d93c
Mirko <mirkocaccavone03@gmail.com>
2025-03-25 16:20:49 +0100

    [feat] add image path middleware and update static file serving

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const port = process.env.PORT;
 
 
 // importo i middleware
+import imagePathMiddleware from './middleware/imagePath.js';
 import notFound from './middleware/notFound.js';
 import errorHandler from './middleware/errorHandler.js';
 
@@ -39,7 +40,10 @@ app.use(express.json());
 
 
 // Serve i file statici dalla cartella 'public'
-app.use(express.static('public/images'));
+app.use(express.static('public'));
+
+// registro  il middleware del path delle img
+app.use(imagePathMiddleware);
 
 
 
