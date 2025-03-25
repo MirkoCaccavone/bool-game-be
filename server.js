e427a1730531f7a1a564407b8dee7092d53ccbb0
nicolo_canevari <nico.canevari@outlook.it>
2025-03-25 09:37:35 +0100

    aggiunte rotte cart e order

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ import express from 'express';
 // Importo cors per abilitare le richieste cross-origin
 import cors from 'cors';
 
-// Importo le rotte per i pagamenti
-// import paymentRoutes from './routes/payments.js';
-
 // Importo la rotta del Carrello per lo shopping
 import cartRoutes from './routes/cartRoutes.js'
 
@@ -22,7 +19,6 @@ const app = express();
 
 const port = process.env.PORT;
 
-
 // importo i middleware
 import notFound from './middleware/notFound.js';
 import errorHandler from './middleware/errorHandler.js';
@@ -30,32 +26,39 @@ import errorHandler from './middleware/errorHandler.js';
 // importo le routes
 import productRoutes from './routes/productRoutes.js';
 
+// Rotte del carrello
+import cartRoutes from './routes/cartRoute.js';
+
+// Rotte degli ordini
+import orderRoutes from './routes/orderRoutes.js';
+
+// Importo le rotte per i pagamenti
+// import paymentRoutes from './routes/payments.js';
 
 app.use(cors({ origin: process.env.FE_APP }))
 
+// Rotte per il carrello
+app.use('/api/cart', cartRoutes);
 
+// Rotte per gli ordini
+app.use('/api/orders', orderRoutes);
+
+app.use('/api/products', productRoutes);
+
+// Imposto il percorso per le API di pagamento
+// app.use('/api/payments', paymentRoutes);
 
 // registro il body-parser per "application/json"
 // interpreta quello che sarà passato come file JSON
 app.use(express.json());
 
-
-
 // Serve i file statici dalla cartella 'public'
 app.use(express.static('public'));
-
-// Importo il percorso per le API del carrello (cart)
-app.use('/api/cart', cartRoutes);
-
-// Imposto il percorso per le API di pagamento
-// app.use('/api/payments', paymentRoutes);
 
 // Gestisce la route principale ('/')
 app.get('/api', (req, res) => {
     res.send("Server di boolGame")
 });
-
-app.use('/api/products', productRoutes);
 
 // utilizzo middleware di gestione not found 404
 app.use(notFound);
